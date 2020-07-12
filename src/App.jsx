cb606820291aab22ea05a6425d508b25bb89f3fa
floriansr <sueurfloriansf@gmail.com>
2020-07-12 18:57:15 +0200

    [create] projects page initialization

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
 import Navbar from 'components/Navbar';
+import Projects from 'pages/Projects';
 
 const App = () => {
   return (
@@ -14,7 +15,7 @@ const App = () => {
             <Route exact path="/new-playlist" />
             <Route exact path="/about" />
             <Route exact path="/project/:projectId" />
-            <Route exact path="/projects" />
+            <Route exact path="/projects" component={Projects} />
             <Route path="*" status={404} />
           </Switch>
         </div>
